06a052d4cec95ab290fead5ab380dc8159f1cd96
Piotr Dobrowolski <dobrypd@gmail.com>
2012-05-09 09:28:54 +0200

    5th task scratch

---
+++ 5/task5.py
@@ -0,0 +1,26 @@
+"""
+Piotr Dobrowolski
+(291528)
+Task 5
+"""
+
+class 
+
+def find_function(fasta_file):
+    #load sequences from fasta file
+    #chose random sequence
+    
+    s = Seq...
+    print "Chosen sequence: "
+    print s
+    
+    #make DNAs normal, reversed, * 3 frames (only using frames)
+
+    #search function by PFAM WITH BLAST
+    b = []
+    #create frames
+    bf = []
+    #search function by PFAM WITH FRAMES
+
+    #multiply b, bf
+    return (b, bf, mul(b, bf))
